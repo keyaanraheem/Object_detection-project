8f1824f877536b3655e341d56e7a7dbbfebaaaa8
keyaanraheem <121084515+keyaanraheem@users.noreply.github.com>
2024-04-07 11:53:23 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function Bunny()
+{
+    window.location ="bunny.html"
+}
+
+function Cat()
+{
+    window.location ="cat.html"
+}
+
+function Dog()
+{
+    window.location ="dog.html"
+}
+
+function Parrot()
+{
+    window.location ="parrot.html"
+}
